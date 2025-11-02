49ed26d138445a8a2250401945d17131a5307682
sumin <sumin50240585@gmail.com>
2025-11-02 23:42:15 +0900

    feat[ErrorMessage.kt]: 에러 메세지 상수 정의

--- src/main/kotlin/lotto/common/ErrorMessage.kt
+++ src/main/kotlin/lotto/common/ErrorMessage.kt
@@ -5,5 +5,4 @@ object ErrorMessage {
     const val DUPLICATE_NUMBER = "[ERROR] 중복된 번호가 있습니다."
     const val OUT_OF_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
     const val INVALID_AMOUNT = "[ERROR] 구입 금액은 1000원 단위여야 합니다."
-    
 }
